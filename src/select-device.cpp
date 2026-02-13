769db20aab6830f0723074431ed44601bf3a8272
SublimiRea <94312481+HolyNaet@users.noreply.github.com>
2026-02-13 13:29:00 +0700

    better remove the old cruft

--- src/select-device.cpp
+++ src/select-device.cpp
@@ -5,41 +5,12 @@
 #include <cstdlib>
 
 #include "../include/nvml.h"
-// #include "./printing.cpp"
-
-// int select_device_index(const unsigned int dev_count) {
-//   int devIdx = -1;
-//
-//   while (1) {
-//     for (unsigned int i = 0; i < dev_count; ++i) printDevice(i);
-//
-//     printf("Choose which device to select: ");
-//     scanf("%d", &devIdx);
-//
-//     if (devIdx >= 0 && devIdx < dev_count) break;
-//     printf("Invalid Device ID, try again.\n");
-//   }
-//
-//   return devIdx;
-// }
 
 int get_uuid(char *uuid) {
   int gpu_idx = 0;
-  // unsigned int device_count = 0;
 
   nvmlDevice_t device;
   nvmlReturn_t ret;
-
-  // code = nvmlDeviceGetCount_v2(&device_count);
-  // if (!device_count || code != NVML_SUCCESS) return EXIT_FAILURE;
-
-  // SLI is dead, typical laptops or desktop setups only have one dGPU
-  // NVML is smart enough anyway, is it even worth implementing this?
-  // if (device_count > 1) {
-  //   printf("\nMultiple GPU devices detected.\n");
-  //   gpu_idx = select_device_index(device_count);
-  // }
-  // if (gpu_idx == -1) return EXIT_FAILURE;
 
   ret = nvmlDeviceGetHandleByIndex_v2(gpu_idx, &device);
   if (ret != NVML_SUCCESS) return EXIT_FAILURE;
